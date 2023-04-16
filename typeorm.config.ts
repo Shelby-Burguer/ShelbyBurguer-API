720d5631676f49f6d0fd901785ff7992bfa0aef4
Yorfrank Bastidas <44125021+YkBastidas@users.noreply.github.com>
2023-04-16 19:13:38 -0400

    typeorm stuff

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -8,7 +8,6 @@ import { igdt_pdtEntity } from './src/producto_combo/infraestructure/orm/igdt_pd
 import { ingredienteEntity } from './src/producto_combo/infraestructure/orm/ingrediente.orm';
 import { pdt_cbEntity } from './src/producto_combo/infraestructure/orm/pdt_cb.orm';
 import { productoEntity } from './src/producto_combo/infraestructure/orm/producto.orm';
-import { migr_carrito_1678815849795 } from './migrations/1678815849795-migr_carrito';
 import { carritoEntity } from './src/carrito/infraestructure/entities/carrito.orm';
 import { OrdenEntity } from './src/orden/infraestructure/entities/orden.orm';
 import { pdtcb_odEntity } from './src/orden/infraestructure/entities/pdtcb_od.orm';
@@ -37,5 +36,5 @@ export default new DataSource({
     OrdenEntity,
     pdtcb_odEntity,
   ],
-  migrations: [migr_carrito_1678815849795],
+  migrations: [],
 });
